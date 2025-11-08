0df40da9f97bffb465ea17c38b1eb512d21e23f0
nakamuray41104-byte <235420323+nakamuray41104-byte@users.noreply.github.com>
2025-11-08 12:01:59 +0000

    Fix all 199 RSpec tests - 100% pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,26 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
-  belongs_to :books
+  has_many :books, dependent: :destroy
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-
-  
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
   end
 end
