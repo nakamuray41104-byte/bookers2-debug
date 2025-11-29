efe12caf0c22ea7d4e617e241c22d36825ddadf8
nakamuray41104-byte <235420323+nakamuray41104-byte@users.noreply.github.com>
2025-11-29 05:55:45 +0000

    課題6完了: いいね機能とコメント機能を非同期通信で実装 実装内容: - いいねボタンを部分テンプレート化 - いいね機能を非同期化(Ajax対応) - コメント一覧を部分テンプレート化 - コメントフォームを部分テンプレート化 - コメント機能を非同期化(Ajax対応) - コメント投稿後フォームを自動クリア - form_withでlocal: false使用

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,24 +1,21 @@
 class BookCommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @book = Book.find(params[:book_id])
-    @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = @book.id
-    @book_comment.save
-    redirect_back(fallback_location: root_path)
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
   end
-  
+
   def destroy
-    @book_comment = BookComment.find(params[:id])
-    if @book_comment.user == current_user
-      @book_comment.destroy
-    end
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
   end
-  
+
   private
-  
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
